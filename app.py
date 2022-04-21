dd0acec476fa7f4f0207b83ad5640a82f52a7bc0
unknown <fouadfouad72@gmail.com>
2022-04-21 20:48:10 +0300

    release

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-!pip install my_package
+from flask import flask ,jsonify,request
+import time 
+app =flask (__name__)
+@app.route("/bot",'/post')
+def response():
+    query =dict (request.form)['query']
+    result =query + "fofofofofo"+time.ctime()
+    return jsonify ({"response":result})
+
+    if __name__ =="__main__":
+        app.run(host="0.0.0.0",)
 
